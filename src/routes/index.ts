b4fc64c8b6fc3f41c35dc863dfb7351f7e8a0f84
A-Tirta <tirtananda68@gmail.com>
2022-03-01 11:28:53 +0700

    Update routes-index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,17 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = Router();
 
-import status from "../controller/status";
-routes.use("/status", status);
+import status from '../controller/status';
+routes.use('/status', status);
+
+import user from '../controller/user';
+routes.use('/user', user);
+
+import comments from '../controller/comments';
+routes.use('/comments', comments);
+
+import recepies from '../controller/recepies';
+routes.use('/recepies', recepies);
 
 export default routes;
